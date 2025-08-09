00b70c7aaf0b1971f24ca5b05ad45d6cfe69c20c
oveldman <oveldman@gmail.com>
2025-08-09 12:29:12 +0200

    Add saving in the database

--- src/MadWorldNL.MadTransfer.Infrastructures.Databases/Files/FileRepository.cs
+++ src/MadWorldNL.MadTransfer.Infrastructures.Databases/Files/FileRepository.cs
@@ -1,9 +1,25 @@
+using MadWorldNL.MadTransfer.Web;
+
 namespace MadWorldNL.MadTransfer.Files;
 
-public sealed class FileRepository : IFileRepository
+public sealed class FileRepository(MadTransferContext context) : IFileRepository
 {
     public void Add(UserFile userFile)
     {
-        //throw new NotImplementedException();
+        var fileFound = GetUserFile(userFile.Url);
+
+        if (fileFound != null)
+        {
+            // TODO: Add better exception
+            throw new Exception();
+        }
+        
+        context.Add(userFile);
+        context.SaveChanges();
+    }
+
+    private UserFile? GetUserFile(Hyperlink url)
+    {
+        return context.Files.FirstOrDefault(f => f.Url.Equals(url));
     }
 }
