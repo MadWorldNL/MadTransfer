bb89889d476c001c16a6324c210864d8f1bb1883
oveldman <oveldman@gmail.com>
2025-08-11 19:13:19 +0200

    First test succeed.

--- src/MadWorldNL.MadTransfer.Controllers.Api.IntegrationTests/Common/ApiWebApplicationFactory.cs
+++ src/MadWorldNL.MadTransfer.Controllers.Api.IntegrationTests/Common/ApiWebApplicationFactory.cs
@@ -1,8 +1,46 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Testcontainers.PostgreSql;
 
 namespace MadWorldNL.MadTransfer.Common;
 
 public class ApiWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private const string DbUserName = "TestDbUser";
+    private const string DbPassword = "TestDbPassword";
     
+    private readonly PostgreSqlContainer _postgresContainer =
+        new PostgreSqlBuilder()
+            .WithUsername(DbUserName)
+            .WithPassword(DbPassword)
+            .Build();
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        _postgresContainer.StartAsync().GetAwaiter().GetResult();
+        
+        builder.ConfigureHostConfiguration(config =>
+        {
+            var testSettings = new Dictionary<string, string?>
+            {
+                ["DatabaseSettings:Host"] = _postgresContainer.Hostname,
+                ["DatabaseSettings:Port"] = _postgresContainer.GetMappedPublicPort(5432).ToString(),
+                ["DatabaseSettings:User"] = DbUserName,
+                ["DatabaseSettings:Password"] = DbPassword
+            };
+
+            config.AddInMemoryCollection(testSettings);
+        });
+        return base.CreateHost(builder);
+    }
+
+    public override async ValueTask DisposeAsync()
+    {
+        await _postgresContainer.DisposeAsync();
+        await base.DisposeAsync();
+        
+        GC.SuppressFinalize(this);
+    }
 }
--- src/MadWorldNL.MadTransfer.Controllers.Api.IntegrationTests/Endpoints/Debug/GetAnonymousTests.cs
+++ src/MadWorldNL.MadTransfer.Controllers.Api.IntegrationTests/Endpoints/Debug/GetAnonymousTests.cs
@@ -3,15 +3,13 @@ using Shouldly;
 
 namespace MadWorldNL.MadTransfer.Endpoints.Debug;
 
-public class GetAnonymousTests(ApiWebApplicationFactory factory) : IClassFixture<ApiWebApplicationFactory>
+public sealed class GetAnonymousTests(ApiWebApplicationFactory factory) : IClassFixture<ApiWebApplicationFactory>
 {
-    private readonly ApiWebApplicationFactory _factory = factory;
-
     [Fact]
     public async Task Get_WhenGivenAnEmptyRequest_ReturnsOk()
     {
         // Arrange
-        using var client = _factory.CreateClient();
+        using var client = factory.CreateClient();
         
         // Act
         var response = await client.GetAsync("/Debug/Anonymous");
@@ -20,6 +18,6 @@ public class GetAnonymousTests(ApiWebApplicationFactory factory) : IClassFixture
         response.EnsureSuccessStatusCode();
         
         var content = await response.Content.ReadAsStringAsync();
-        content.ShouldBe("OK");
+        content.ShouldBe("\"OK\"");
     }
 }
