9f4ebd786d2505ccf1fe616ea3928a0368957c0d
oveldman <oveldman@gmail.com>
2025-09-02 22:57:48 +0200

    Extract authentication setup into reusable extension method

--- src/MadWorldNL.MadTransfer.Controllers.Api/Program.cs
+++ src/MadWorldNL.MadTransfer.Controllers.Api/Program.cs
@@ -35,58 +35,7 @@ if (builder.Configuration.GetValue<bool>("SerilogSettings:Active"))
 }
 
 builder.AddOpenTelemetryForDevelopment();
-
-var authenticationSettings = builder.Configuration
-    .GetRequiredSection(AuthenticationSettings.Key)
-    .Get<AuthenticationSettings>()!;
-
-builder.Services.AddSingleton(Options.Create(authenticationSettings));
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.Authority = authenticationSettings.Authority; // Keycloak realm URL
-        options.Audience = authenticationSettings.Audience; // Match the "Client ID" of your Keycloak client
-        options.RequireHttpsMetadata = false; // Only for dev/local
-
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = authenticationSettings.ValidateUser,
-            ValidateAudience = authenticationSettings.ValidateUser,
-            ValidateLifetime = authenticationSettings.ValidateUser,
-            ValidateIssuerSigningKey = authenticationSettings.ValidateUser,
-            NameClaimType = "preferred_username",
-            RoleClaimType = "roles"
-        };
-
-        if (!authenticationSettings.ValidateUser)
-        {
-            options.TokenValidationParameters.SignatureValidator = (token, _) =>
-            {
-                // Just return the token without validating signature
-                var handler = new JsonWebTokenHandler();
-                return handler.ReadJsonWebToken(token);
-            };
-        }
-        
-        options.Events = new JwtBearerEvents
-        {
-            OnAuthenticationFailed = context =>
-            {
-                Log.Logger.Information("Authentication failed: {ContextException}", context.Exception);
-                return Task.CompletedTask;
-            },
-            OnTokenValidated = context =>
-            {
-                Log.Logger.Information("Token validated for: {IdentityName}", context.Principal?.Identity?.Name ?? "Unknown");
-                return Task.CompletedTask;
-            }
-        };
-    });
-
-builder.Services.AddAuthorization();
+builder.AddDefaultAuthentication();
 
 builder.Services.AddRateLimiter(options =>
 {
